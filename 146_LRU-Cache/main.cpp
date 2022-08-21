c38b1b7fe8422b9265c794b69964ae0f70755b3a
xiao <berumottoX@163.com>
2022-08-22 01:28:42 +0800

    Initial commit

---
+++ 146_LRU-Cache/main.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct LRULinkNode {
+    int key;
+    int value;
+    struct LRULinkNode *next;
+    struct LRULinkNode *prev;
+} LRULinkNodeStru;
+
+class LRUCache {
+public:
+    void putShow(int key) {
+        LRULinkNodeStru *flag = head;
+
+        cout << "Put key: " << key << endl;
+        while (flag != nullptr) {
+            cout << "key = " << flag->key << ", value = " << flag->value << endl;
+            flag = flag->next;
+        }
+        cout << endl;
+    }
+
+    void getShow(int key) {
+        LRULinkNodeStru *flag = head;
+
+        cout << "Get key: " << key << endl;
+        while (flag != nullptr) {
+            cout << "key = " << flag->key << ", value = " << flag->value << endl;
+            flag = flag->next;
+        }
+        cout << endl;
+    }
+
+    LRUCache(int capacity) {
+        LRUCacheCap = capacity;
+        LRUCacheLen = 0;
+        head = new(LRULinkNodeStru);
+        head->key = 0;
+        head->value = 0;
+        head->next = nullptr;
+        head->prev = nullptr;
+        rear = head;
+    }
+
+    int get(int key) {
+        LRULinkNodeStru *flag = head;
+
+        while (flag != nullptr) {
+            if ((flag != head) && (flag->key == key)) {
+                if(flag != rear) {
+                    flag->prev->next = flag->next;
+                    flag->next->prev = flag->prev;
+                    flag->prev->next = flag->next;
+                    flag->next = rear->next;
+                    rear->next = flag;
+                    flag->prev = rear;
+                    rear = flag;
+                }
+                return flag->value;
+            }
+            flag = flag->next;
+        }
+//        getShow(key);
+        return -1;
+    }
+
+    void put(int key, int value) {
+        LRULinkNodeStru *flag = head;
+
+        while (flag != nullptr) {
+            if ((flag != head) && (flag->key == key)) {
+                flag->value = value;
+                if(flag != rear) {
+                    flag->prev->next = flag->next;
+                    flag->next->prev = flag->prev;
+                    flag->prev->next = flag->next;
+                    flag->next = rear->next;
+                    rear->next = flag;
+                    flag->prev = rear;
+                    rear = flag;
+                }
+                return;
+            }
+            flag = flag->next;
+        }
+
+        flag = head->next;
+//        cout << "key = " << key << ", LRUCacheLen = " << LRUCacheLen << endl;
+        if (LRUCacheLen == LRUCacheCap) {
+            if(flag != nullptr) {
+                if (flag->next != nullptr) {
+                    flag->next->prev = flag->prev;
+                }
+                flag->prev->next = flag->next;
+                rear = rear == flag ? flag->prev : rear;
+                LRUCacheLen -= 1;
+                delete flag;
+            }
+        }
+
+        LRULinkNodeStru *newNode = nullptr;
+        newNode = new(LRULinkNodeStru);
+        newNode->key = key;
+        newNode->value = value;
+        newNode->next = nullptr;
+        newNode->prev = rear;
+        rear->next = newNode;
+        rear = newNode;
+        LRUCacheLen = LRUCacheLen == LRUCacheCap ? LRUCacheCap : LRUCacheLen + 1;
+//        putShow(key);
+        return;
+    }
+
+private:
+    int LRUCacheCap;
+    int LRUCacheLen;
+    LRULinkNodeStru *head;
+    LRULinkNodeStru *rear;
+};
+
+int main() {
+    LRUCache *lRUCache = new LRUCache(1);
+
+    lRUCache->put(1, 1); // 缓存是 {1=1}
+    lRUCache->put(2, 2); // 缓存是 {1=1, 2=2}
+    cout << lRUCache->get(1) << ", ";    // 返回 1
+    lRUCache->put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+    cout << lRUCache->get(2) << ", ";    // 返回 -1 (未找到)
+    lRUCache->put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+    cout << lRUCache->get(1) << ", ";    // 返回 -1 (未找到)
+    cout << lRUCache->get(3) << ", ";    // 返回 3
+    cout << lRUCache->get(4) << endl;;    // 返回 4
+    return 0;
+}
